a690bc5696afdc061a9f8cb5df153359cc5c2844
IamVatsal <vatsalpatel0609@gmail.com>
2025-05-02 15:55:30 +0530

    alt for 6.5

--- Practical 6/6.5/index.c
+++ Practical 6/6.5/index.c
@@ -2,19 +2,14 @@
 
 int main(){
     int a[3][3]= {{1,2,3},{4,5,6},{7,8,9}};
-    int temp;
-    for(int i = 0 ; i < 3; i++){
-        for(int j = i + 1; j < 3; j++){
-            temp = a[i][j];
-            a[i][j] = a[j][i];
-            a[j][i] = temp;
-        }
-    }
+
+
     for (int i = 0 ; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            printf("%d ",a[i][j]);
+            printf("%d ",a[j][i]);
         }
         printf("\n");
     }
+    
     return 0;
 }
