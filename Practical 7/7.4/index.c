606032a4246d0321aa170f5ad06fbba752f0f15d
IamVatsal <vatsalpatel0609@gmail.com>
2025-05-03 23:04:58 +0530

    update

--- Practical 7/7.4/index.c
+++ Practical 7/7.4/index.c
@@ -7,8 +7,8 @@ int main(){
     double n = 10;
     double factValue = fact(n);
     double factValue2 = factWithLoop(n);
-    printf("The Factorial value is: %lf\n", factValue);
-    printf("The Factorial value With Loop is: %lf\n", factValue2);
+    printf("The Factorial value is: %.2lf\n", factValue);
+    printf("The Factorial value With Loop is: %.2lf\n", factValue2);
     return 0;
 }
 
@@ -25,10 +25,10 @@ double factWithLoop(int n){
         return 1.0;
     }
 
-    double sum = 1;
+    double proud = 1;
 
-    for (int i = n - 1; i > 1; i--){
-        sum *= i;
+    for (int i = n; i > 1; i--){
+        proud *= i;
     }
-    return sum;
+    return proud;
 }
